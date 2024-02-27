357b15a855ae7c0d15850cfb0429351ce3764672
fregas <me@craigscode.com>
2024-02-27 11:18:08 -0600

    working w/ refs

--- rust-book/what-is-ownership/ownership_main.rs
+++ rust-book/what-is-ownership/ownership_main.rs
@@ -1,26 +1,28 @@
 
 fn main() {
-  let s = String::from("hello");  // s comes into scope
+  let s1 = gives_ownership();
 
-  takes_ownership(s);             // s's value moves into the function...
-                                  // ... and so is no longer valid here
+  println!("s1 {}", s1);
 
-  println!("{}", s);
+  let s2 = String::from("hello");
 
-  let x = 5;                      // x comes into scope
+  println!("s2 {}", s2);
 
-  makes_copy(x);                  // x would move into the function,
-                                  // but i32 is Copy, so it's okay to still
-                                  // use x afterward
+  let s2 = takes_and_gives_back(s2);
 
-  println!("{}", x);                                    
+  println!("s3 {}", s2);
+
 
 }
 
-fn takes_ownership(some_string: String) {
-  println!("{}", some_string);
+
+fn gives_ownership() -> String {
+  let some_string = String::from("yours");
+
+  some_string
 }
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
-  println!("{}", some_integer);
-} 
+fn takes_and_gives_back(a_string: String) -> String {
+  a_string
+}
+
